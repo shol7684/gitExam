9da7a58d68fa80c59a08f925ac29ef91f369f6a6
sumin <shol7684@naver.com>
2022-03-15 16:35:24 +0900

    첫번째 커밋

---
+++ src/Lion.java
@@ -0,0 +1,8 @@
+
+public class Lion {
+	String team = "Lions";
+	
+	String manager = "Mary";
+	
+	String[] member = {"Thomas", "Karen", "Margaret"};
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	private String name = "sumin";
+	
+	
+	public static void main(String[] args) {
+		System.out.println("main");
+		
+		System.out.println("코드 추가");
+		
+	}
+	
+	
+	
+	
+
+}
